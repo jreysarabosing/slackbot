0d39bd0e9992553cc1d4338324bfce9796fd6c3a
Sarabosing, J. A <j.a.sarabosing@accenture.com>
2020-11-09 22:26:05 +0800

    added censoring and scheduled messaging

--- slackBot.py
+++ slackBot.py
@@ -1,9 +1,14 @@
 import os
 import slack
+import string
 from pathlib import Path
 from dotenv import load_dotenv
 from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
+from datetime import datetime, timedelta
+import pprint
+
+printer = pprint.PrettyPrinter()
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -16,6 +21,13 @@ BOT_ID = client.api_call('auth.test')['user_id']
 
 message_counts = {}
 welcome_messages = {}
+
+RESTRICTED_WORDS = ['fuck', 'shit', 'crap']
+
+SCHEDULED_MESSAGES = [
+	{'text': 'First message!', 'post_at': (datetime.now() + timedelta(seconds=25)).timestamp(), 'channel': 'C01CHP6TGET'},
+	{'text': 'Second message!', 'post_at': (datetime.now() + timedelta(seconds=30)).timestamp(), 'channel': 'C01CHP6TGET'}
+]
 
 class WeclomeMessage:
 	START_TEXT = {
@@ -59,13 +71,42 @@ class WeclomeMessage:
 		return {'type': 'section', 'text': {'type': 'mrkdwn', 'text': text}}
 
 def send_welcome_message(channel, user):
+	if channel not in welcome_messages:
+		welcome_messages[channel] = {}
+	if user in welcome_messages[channel]:
+		return
+
 	welcome = WeclomeMessage(channel, user)
 	message = welcome.get_message()
 	response = client.chat_postMessage(**message)
 	welcome.timestamp = response['ts']
-	if channel not in welcome_messages:
-		welcome_messages[channel] = {}
+
 	welcome_messages[channel][user] = welcome
+
+def check_restricted_word(message):
+	msg = message.lower()
+	msg = msg.translate(str.maketrans('', '', string.punctuation))
+	return any(word in msg for word in RESTRICTED_WORDS)
+
+def schedule_messages(messages):
+	for message in messages:
+		response = client.chat_scheduleMessage(channel=message['channel'], text=message['text'], post_at=message['post_at']).data
+
+def delete_scheduled_messages(ids, channel):
+	for _id in ids:
+		try:
+			client.chat_deleteScheduledMessage(channel=channel, scheduled_message_id=_id)
+		except Exception as e:
+			print(e)
+
+def list_scheduled_messages(channel):
+	response = client.chat_scheduledMessages_list(channel=channel)
+	messages = response.data.get('scheduled_messages')
+	ids = []
+	for msg in messages:
+		ids.append(msg.get('id'))
+
+	return ids
 
 @slack_event_adapter.on('message')
 def message(payload):
@@ -78,12 +119,13 @@ def message(payload):
 			message_counts[user_id] += 1
 		else:
 			message_counts[user_id] = 1
-
 		if text.lower() == 'start':
 			send_welcome_message(f'@{user_id}', user_id)
-
-		if text.find('69') != -1:
+		elif text.find('69') != -1:
 			client.chat_postMessage(channel=channel_id, text='nice')
+		elif check_restricted_word(text):
+			ts = event.get('ts')
+			client.chat_postMessage(channel=channel_id, thread_ts=ts, text='Some words you typed are restricted. Please refrain from using such language.')
 
 @slack_event_adapter.on('reaction_added')
 def reaction(payload):
@@ -111,4 +153,7 @@ def message_count():
 	return Response(), 200
 
 if __name__ == '__main__':
+	schedule_messages(SCHEDULED_MESSAGES)
+	ids = list_scheduled_messages('C01CHP6TGET')
+	delete_scheduled_messages(ids, 'C01CHP6TGET')
 	app.run(debug=True)
